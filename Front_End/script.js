8999a72cebc7b5c6ab0696b78b977a34a8578c30
Ritesh Daud <riteshdaud464@gmail.com>
2026-03-12 18:15:44 +0530

    addded

--- Front_End/script.js
+++ Front_End/script.js
@@ -1,7 +1,35 @@
-function login() {
-    alert("Login feature coming soon!");
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price, image){
+
+let product = {
+name:name,
+price:price,
+image:image
+};
+
+cart.push(product);
+
+localStorage.setItem("cart", JSON.stringify(cart));
+
+alert(name + " added to cart");
 }
 
-function addToCart(product) {
-    alert(product + " added to cart!");
+function loadCart(){
+
+let cartItems = JSON.parse(localStorage.getItem("cart")) || [];
+
+let container = document.getElementById("cartItems");
+
+cartItems.forEach(item => {
+
+container.innerHTML += `
+<div class="cart-card">
+<img src="${item.image}">
+<h3>${item.name}</h3>
+<p>₹${item.price}</p>
+</div>
+`;
+
+});
 }
